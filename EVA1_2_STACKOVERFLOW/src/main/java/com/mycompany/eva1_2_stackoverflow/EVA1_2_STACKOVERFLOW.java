96aabcd641c344fa7c3e98577c1f36f24bf8f760
gene ricardo lazarin nuñez <159508631+genelazarin86@users.noreply.github.com>
2025-01-30 17:40:21 -0600

    Add files via upload

---
+++ EVA1_2_STACKOVERFLOW/src/main/java/com/mycompany/eva1_2_stackoverflow/EVA1_2_STACKOVERFLOW.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_2_stackoverflow;
+
+/**
+ *
+ * @author Admin
+ */
+public class EVA1_2_STACKOVERFLOW {
+
+    public static void main(String[] args) {
+    A();
+    }
+    public static void A (){
+    A();
+    }
+}
---
+++ EVA_1_MANEJO_MEMORIA/src/main/java/com/mycompany/eva_1_manejo_memoria/EVA_1_MANEJO_MEMORIA.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva_1_manejo_memoria;
+
+/**
+ *
+ * @author Admin
+ */
+public class EVA_1_MANEJO_MEMORIA {
+    
+
+    public static void main(String[] args) {
+        System.out.println("inicia main");
+        
+        A();
+        System.out.println("termina main");
+    }
+       public static void A(){
+           System.out.println("inicia a ");
+       B();
+           System.out.println("termina a");
+       }
+       public static void B(){
+           System.out.println("inicia b");
+           System.out.println("termina b");
+       }
+    }
+
+
