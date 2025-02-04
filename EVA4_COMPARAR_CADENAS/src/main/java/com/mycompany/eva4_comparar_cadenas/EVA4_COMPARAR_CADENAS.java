8b987c864514caf69458209651931ab301dd96f0
gene ricardo lazarin nuñez <159508631+genelazarin86@users.noreply.github.com>
2025-02-04 11:04:59 -0600

    Add files via upload

---
+++ EVA4_COMPARAR_CADENAS/src/main/java/com/mycompany/eva4_comparar_cadenas/EVA4_COMPARAR_CADENAS.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva4_comparar_cadenas;
+
+/**
+ *
+ * @author Admin
+ */
+public class EVA4_COMPARAR_CADENAS {
+
+    public static void main(String[] args) {
+   String cade1 = "HOLA";
+   String cade2 = "HOLA";
+   if( cade1 == cade2)
+            System.out.println("cadenas iguales");
+   
+   else System.out.println("cadenas diferentes");
+    }
+}
